3b81c568adbc1c21291946cba948acbe96aaadf3
Adeyinka Junaid <yinkajunn@gmail.com>
2022-12-31 12:03:09 +0100

    Add fetch API for profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,22 +12,41 @@ const Home = () => {
         inputStyle = 'input'
     }
 
+    const changeInputHandler = (event) => {
+        // console.log(event)
+        setProfileName(event.target.value)
+        // console.log(profileName)
+    }
+
+    const getUserInfo = () => {
+        event.preventDefault()
+        console.log(profileName);
+        fetch(`https://api.github.com/users/${profileName}`)
+        .then (response => response.json())
+        .then (data => console.log(data))
+    }
+
+
+
+
     return (
         <>
             <div className={styles['text-area']}>
                 <h1>Hi Dev.</h1>
                 <h2>Check your Github stats</h2>
             </div>
+
             <div className={styles['input-area']}>
-                <div className={styles['input-div']}>
-                    {/* <input name='profilename' type="text" placeholder="What's your profile name" /> */}
-                    <input name='profilename' type="text" />
-                    <label className={styles['input-label']} htmlFor="profilename">
-                        Github Profile Name
-                    </label>
-                </div>
-                <button>Get Details</button>
-                <button className='btn btn-primary'>Bootstrap test</button>
+                <form onSubmit={getUserInfo}>
+                    <div className={styles['input-div']}>
+                        {/* <input name='profilename' type="text" placeholder="What's your profile name" /> */}
+                        <input onChange={changeInputHandler} name='profilename' placeholder="What's your profile name" type="text" />
+                        <label className={styles['input-label']} htmlFor="profilename">
+                            Github Profile Name
+                        </label>
+                    </div>
+                    <button>Get Details</button>
+                </form>
             </div>
         </>
     )
