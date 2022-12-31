2bc33371677443834505128ec346756c422438ed
Adeyinka Junaid <yinkajunn@gmail.com>
2022-12-31 10:11:44 +0100

    Add Bootstrap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,18 +3,31 @@ import { useState } from 'react'
 import styles from './Home.module.css'
 
 const Home = () => {
-    const[pprofileName, setProfileName] = useState(null)
+    const [profileName, setProfileName] = useState(null)
 
+    let inputStyle
+    if (profileName === '') {
+        inputStyle = 'invalid-input'
+    } else {
+        inputStyle = 'input'
+    }
 
     return (
         <>
-            <div>
+            <div className={styles['text-area']}>
                 <h1>Hi Dev.</h1>
                 <h2>Check your Github stats</h2>
             </div>
             <div className={styles['input-area']}>
-                <input type="text" placeholder="What's your profile name" />
+                <div className={styles['input-div']}>
+                    {/* <input name='profilename' type="text" placeholder="What's your profile name" /> */}
+                    <input name='profilename' type="text" />
+                    <label className={styles['input-label']} htmlFor="profilename">
+                        Github Profile Name
+                    </label>
+                </div>
                 <button>Get Details</button>
+                <button className='btn btn-primary'>Bootstrap test</button>
             </div>
         </>
     )
